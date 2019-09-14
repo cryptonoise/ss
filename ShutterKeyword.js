830aedd973c7bef6a93482662fe431f40be8177c
cryptonoise <markmelikead@gmail.com>
2019-09-15 00:11:06 +0300

    Update ShutterKeyword.js

--- ShutterKeyword.js
+++ ShutterKeyword.js
@@ -2,7 +2,7 @@
 // @name          Shutterstock Keywords Pizding
 // @author        Freem
 // @source        https://raw.githubusercontent.com/cryptonoise/ss/master/ShutterKeyword.js
-// @version       0.1
+// @version       0.2
 // @description   Pizding image keywords
 // @include       https://www.shutterstock.com/image-photo/*
 // @include       https://www.shutterstock.com/image-vector/*
@@ -10,7 +10,7 @@
 // ==/UserScript==
 
 (() => {
-
+  
     setTimeout(function() {
 
     let keywords = document.querySelectorAll('.C_a_c')[0].children[0].children[0].children,
@@ -18,20 +18,19 @@
         keys = document.querySelector(".C_a_c"),
         parent = keys.parentNode;
 
-    keywords = [...keywords].map(k => k.innerText).map(a => `<a href="/search/${a}">${a}</a>`);
-
+    keywords = [...keywords].map(k => k.innerText).map(a => `${a}`);
     newKeywords.innerHTML = `${keywords.length} keywords:<br>${keywords.join(', ')}`;
     newKeywords.style.cssText = 'padding-left: 14px; text-align: left; line-height: 150%';
 
     parent.appendChild(newKeywords);
     parent.removeChild(keys);
 
-    document.body.innerHTML = document.body.innerHTML.replace('By ', 'Loshara:');
-    document.body.innerHTML = document.body.innerHTML.replace('By', 'Loshara:');
-    document.body.innerHTML = document.body.innerHTML.replace('Related keywords', 'Keywords for pizding');
-    document.body.innerHTML = document.body.innerHTML.replace('Similar images', 'Ostalnoe govno');
+    document.body.innerHTML = document.body.innerHTML.replace('By', 'Loshara:');  
+    document.body.innerHTML = document.body.innerHTML.replace('Related keywords', 'Keywords for pizding');  
+    document.body.innerHTML = document.body.innerHTML.replace('Similar images', 'Ostalnoe govno'); 
 
-    }, 1700);
+    }, 1700); 
 
 })();
+
 
