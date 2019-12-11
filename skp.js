75f108d6704db01dc4dbb469438fe9d90114def5
cryptonoise <markmelikead@gmail.com>
2019-12-11 20:24:35 +0300

    Update skp.js

--- skp.js
+++ skp.js
@@ -1,44 +1,45 @@
 // ==UserScript==
 // @name          SKP
 // @description   Shutterstock Keywords Pizding
-// @version       0.4
+// @version       0.5
 // @author        Freem
 // @source        https://raw.githubusercontent.com/cryptonoise/ss/master/skp.js
 // @icon          https://raw.githubusercontent.com/cryptonoise/ss/master/skpicon.png
-// @include       https://www.shutterstock.com/image-photo/*
-// @include       https://www.shutterstock.com/image-vector/*
-// @include       https://www.shutterstock.com/image-illustration/*
+// @match         https://www.shutterstock.com/*image-photo*
+// @match         https://www.shutterstock.com/*image-vector*
+// @match		      https://www.shutterstock.com/*image-illustration*
+// @match         https://www.shutterstock.com/*video*
+// @require       http://code.jquery.com/jquery-latest.min.js
+// @grant         none
 // ==/UserScript==
 
-(() => {
-  
+'use strict';
+
+var $j = jQuery.noConflict();
+
+$j(document).ready(function() {
     setTimeout(function() {
+        var a = '';
+        $j.each( $j('[data-automation="ExpandableKeywordsList_container_div"'), function(i, left) {
+            $j('a', left).each(function() {
+                a+=$j(this).text()+', ';
+            });
+        });
+        console.log(a);
+        $j('[class="C_a_c"]').css({ height: "100px" });
+        $j('[class="C_a_c"]').html('<div class="row" style="position: relative;height:150px;width:100%;padding: 10px 10px 10px 30px;">' + a + '</div>');
 
-      
-    let keywords = document.querySelectorAll('.C_a_c')[0].children[0].children[0].children,
-        newKeywords = document.createElement("p"),
-        keys = document.querySelector(".C_a_c"),
-        parent = keys.parentNode;
 
-    keywords = [...keywords].map(k => k.innerText).map(a => `${a}`);
-    newKeywords.innerHTML = `${keywords.length} keywords:<br>${keywords.join(', ')}`;
-    newKeywords.style.cssText = 'padding-left: 14px; text-align: left; line-height: 150%';
 
-    parent.appendChild(newKeywords);
-    parent.removeChild(keys);
-
-      
     //Text replacer 
     (function() {
         var replacements, regex, key, textnodes, node, s; 
  
         replacements = { 
-        "Welcome to Shutterstock – images for every project, all with worry-free licensing": "Shutterstock Keywords Pizding запущен!",
-        "Find your plan": "Я ЗНАЮ, ЧТО У ТЕБЯ НА УМЕ, ПРОКАЗНИК 😉",
         "Related keywords": "Keywords for pizding",
         "By": "📷 Успешный стокер:",
         "By ": "📷 Успешный стокер:",
-        "Similar images": "Тебе все еще мало? Ну, вот тогда еще фоточки:",
+        "Similar images": "Симиляры:",
         "Same model": "Та же модель:",
         "Same artist": "Тот же успешный стокер:",
         "Log in": "Ах ты, шалунишка!",
@@ -51,7 +52,6 @@
         for (key in replacements) { 
         regex[key] = new RegExp(key, 'g'); 
     }
- 
         textnodes = document.evaluate( "//body//text()", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null); 
  
     for (var i = 0; i < textnodes.snapshotLength; i++) { 
@@ -64,7 +64,5 @@
     }
     })();
 
-    }, 1800); 
-
-})();
-
+    }, 1900);
+});
