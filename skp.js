5b585a9811438c85dcfa887aadea5e031c22d2f5
cryptonoise <markmelikead@gmail.com>
2020-06-13 00:26:10 +0300

    Update skp.js

--- skp.js
+++ skp.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name          SKP
 // @description   Shutterstock Keywords Pizding
-// @version       1.0
+// @version       1.1
 // @author        Freem
 // @icon          https://raw.githubusercontent.com/cryptonoise/ss/master/skpicon.png
-// @match         https://www.shutterstock.com/*image-photo*
-// @match         https://www.shutterstock.com/*image-vector*
-// @match		      https://www.shutterstock.com/*image-illustration*
-// @match         https://www.shutterstock.com/*video*
-// @require       http://code.jquery.com/jquery-latest.min.js
+// @match					https://www.shutterstock.com/*image-photo*
+// @match					https://www.shutterstock.com/*image-vector*
+// @match					https://www.shutterstock.com/*image-illustration*
+// @match					https://www.shutterstock.com/*video*
+// @exclude				https://www.shutterstock.com/*search*
+// @require				http://code.jquery.com/jquery-latest.min.js
 // @grant         none
 // ==/UserScript==
 
@@ -19,15 +20,15 @@ var $j = jQuery.noConflict();
 
     $j(document).ready(function() {
     		setTimeout(function() {
-        		var a = '';
+        		var key = '';
         		$j.each( $j('.C_a_c'), function(i, left) {
             		$j('a', left).each(function() {
-                		a+=$j(this).text()+', ';
+                		key += $j (this).text() + ', ';
             		});
         		});
-        console.log(a);
+        console.log(key);
         		$j('[class="C_a_c"]').css({ height: "100px" });
-        		$j('[class="C_a_c"]').html('<div class="row" style="position: relative;height:100px;width:100%;color:green;padding: 5px 5px 5px 5px;">' + a + '</div>');   
+        		$j('[class="C_a_c"]').html('<div class="keys" style="position: relative;height:100px;width:100%;color:green;padding: 5px 5px 5px 5px;">' + key + '</div>');   
 
 // Скрыть весь контент
 document.getElementById('content').style.display = 'none';    
@@ -54,7 +55,9 @@ function ButtonClickAction () {
 
 // Вставить ключи в начало страницы       
 let nkeys = document.createElement('div');
-		nkeys.innerHTML = ('<center><div class="keys" style="position: relative;height:100px;width:100%;color:green;padding: 25px 10px 10px 10px;font-family: sans-serif;background-color: #feeff4">' + a + '</div></center>');  
+		nkeys.innerHTML = ('<div class="back" style="background-color: #feeff4; width:100%;"</div>'
+                       + '<center><p class="keys" style="position: relative;height:100px;width:90%;color:green;padding: 25px 10px 10px 10px;font-family: sans-serif;">'
+                       + key + '</p></center>');  
 		document.body.prepend(nkeys);
 		nkeys.before(document.createElement('hr'))
 		nkeys.after(document.createElement('hr'))
@@ -67,7 +70,7 @@ var imageSrc = findimage.getAttribute('src');
 		}       
 let preview = document.createElement('preview');
 		preview.className = "npreview";
-		preview.innerHTML = ('<center><div class="imma" style="background-color: #feeff4;"><img src=' + imageSrc + ' style="margin-top:30px; height:420px" ></div></center>');     
+		preview.innerHTML = ('<center><div class="imma" style="background-color: #feeff4;"><img src=' + imageSrc + ' style="margin:30px 0px 30px 0px; height:420px" ></div></center>');     
     nkeys.before(preview); 
       
 // SKP logo 
@@ -76,7 +79,8 @@ var skp = document.createElement('p');
 var skpem = document.createElement('em');
 		skpem.prepend('SKP'); 
     skp.innerHTML = (
-			'<center><b><H3><div class="nkeystitle" style="color:#cf1d11; padding: 15px 10px 10px 10px;text-shadow: 0 1px 0 #ccc, 0 2px 0 #c9c9c9, 0 3px 0 #bbb, 0 4px 0 #b9b9b9, 0 5px 0 #aaa, 0 6px 1px rgba(0,0,0,.1), 0 0 5px rgba(0,0,0,.1), 0 1px 3px rgba(0,0,0,.3), 0 3px 5px rgba(0,0,0,.2), 0 5px 10px rgba(0,0,0,.25), 0 10px 10px rgba(0,0,0,.2), 0 20px 20px rgba(0,0,0,.15);">' + "Shutterstock Keywords Pizding" + '</div></H3></b></center>'
+			'<center><b><H3><div class="nkeystitle" style="color:#cf1d11; padding: 15px 10px 10px 10px;text-shadow: 0 1px 0 #ccc, 0 2px 0 #c9c9c9, 0 3px 0 #bbb, 0 4px 0 #b9b9b9, 0 5px 0 #aaa, 0 6px 1px rgba(0,0,0,.1), 0 0 5px rgba(0,0,0,.1), 0 1px 3px rgba(0,0,0,.3), 0 3px 5px rgba(0,0,0,.2), 0 5px 10px rgba(0,0,0,.25), 0 10px 10px rgba(0,0,0,.2), 0 20px 20px rgba(0,0,0,.15);">' 
+      + "Shutterstock Keywords Pizding" + '</div></H3></b></center>'
 		);
           
 // Стиль описания фото + его дублирование в начало страницы
@@ -89,7 +93,13 @@ var title = document.querySelector('.m_b_b');
 // Стиль ID
 var photoid = document.querySelector('.m_b_a');
 		photoid.style.cssText = "color: red; font-size:16px;"; 
-          
+
+let search = document.createElement('div');
+		nkeys.innerHTML = ('<div class="back" style="background-color: #feeff4; width:100%;"</div>'
+                       + '<center><p class="keys" style="position: relative;height:100px;width:90%;color:green;padding: 25px 10px 10px 10px;font-family: sans-serif;">'
+                       + a + '</p></center>');       
+     
 //Задержка перед выполнением
     }, 200);
+   
 });
